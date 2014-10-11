0a66021ba8d678bc0aecec2feccb32d9d4a6e66c
zhaozq <apple@ZZQ-Air.local>
2014-10-11 11:26:00 +0800

    Initial Commit

---
+++ ImageFilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageFilter
+//
+//  Created by zhaozq on 14-10-11.
+//  Copyright (c) 2014年 zhaozq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
